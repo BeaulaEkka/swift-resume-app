3fb407e55173b6dc402fe7e91b911573934ad254
Your Name <ekka.beaula@gmail.com>
2025-01-24 23:32:20 +0100

    billing page

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -19,6 +19,7 @@ export default function Navbar() {
 
                 }
             }}/>
+            <UserButton.MenuItems><UserButton.Link/></UserButton.MenuItems>
             </div>
            
     
