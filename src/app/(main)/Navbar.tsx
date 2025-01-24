9eacb4c67a540476a8650e033c26f208c222743a
Your Name <ekka.beaula@gmail.com>
2025-01-24 23:18:22 +0100

    userbutton on nav

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import logo from '@/assets/resume-builder-logo.png'
+import Image from 'next/image'
+import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 export default function Navbar() {
   return (
     <header className='shadow-sm'>
-        <div className='max-w-7xl mx-auto p-3 flex items-center justify-between border border-red-500'>Home</div>
+        <div className='max-w-7xl mx-auto p-3 flex items-center justify-between '>
+            <Link href="/resumes">
+            <div className='flex justify-center items-center gap-2'> <Image src={logo} alt="logo" width={35} height={35}/>
+            <h1 className='font-extrabold text-blue-900 uppercase'><span className=''>Swift</span> AI Resume Builder</h1></div></Link>
+            <UserButton/>
+            </div>
+           
     
       
     </header>
