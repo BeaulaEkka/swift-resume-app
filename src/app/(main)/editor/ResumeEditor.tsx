4682e6046331a2c30dd207ede00af219a7756a7c
Your Name <ekka.beaula@gmail.com>
2025-01-28 14:41:42 +0100

    editor-main

--- src/app/(main)/editor/ResumeEditor.tsx
+++ src/app/(main)/editor/ResumeEditor.tsx
@@ -1,9 +1,35 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 export default function ResumeEditor() {
   return (
     <div className="flex grow flex-col">
-      <h1>Resume editor</h1>ResumeEditor
+      <header className="space-y-1.5 border-b px-3 py-5 text-center">
+        <h1 className="text-2xl font-bold">Design your resume</h1>
+        <p className="text-sm text-muted-foreground">
+          Follow the steps below to create your resume. Your progress will be
+          saved automatically
+        </p>
+      </header>
+      <main className="relative grow h-screen border border-green-500">
+        <div className="absolute bottom-0 top-0 flex w-full  border border-red-500">    vvv</div>
+      </main>
+
+      <footer className="w-full border-t px-3 py-5">
+        <div className="mx-auto flex max-w-7xl flex-wrap justify-between gap-3">
+          <div className="flex items-center gap-3">
+            <Button variant="secondary">Previous Step</Button>
+            <Button variant="secondary">Next Step</Button>
+          </div>
+          <div className="flex items-center gap-3">
+            <Button variant="secondary" asChild>
+              <Link href="/resumes">Close</Link>
+            </Button>
+            <p className="text-muted-foreground opacity-0">Saving...</p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
