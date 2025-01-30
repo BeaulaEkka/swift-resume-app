0c9fff1ffd761b7df7106dc1f48adc7fe000d4d9
Your Name <ekka.beaula@gmail.com>
2025-01-30 13:57:12 +0100

    PersonalInfoForm-start

--- src/app/(main)/editor/ResumeEditor.tsx
+++ src/app/(main)/editor/ResumeEditor.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
 import GeneralInfoForm from "./forms/GeneralInfoForm";
+import PersonalInfoForm from "./forms/PersonalInfoForm";
 
 export default function ResumeEditor() {
   return (
@@ -15,8 +16,9 @@ export default function ResumeEditor() {
       </header>
       <main className="relative min-h-[75vh] grow">
         <div className="absolute bottom-0 top-0 flex w-full">
-          <div className="w-full md:w-1/2 p-3">
+          <div className="w-full p-3 md:w-1/2">
             <GeneralInfoForm />
+            <PersonalInfoForm />
           </div>
           <div className="grow md:border-r" />
           <div className="hidden w-1/2 md:flex">right</div>
