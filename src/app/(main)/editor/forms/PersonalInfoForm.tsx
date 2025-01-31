8acb0fca2bad9955a43c62b107eebd6c95bdd460
Your Name <ekka.beaula@gmail.com>
2025-01-31 23:45:58 +0100

    update

--- src/app/(main)/editor/forms/PersonalInfoForm.tsx
+++ src/app/(main)/editor/forms/PersonalInfoForm.tsx
@@ -1,5 +1,30 @@
+import { personalInfoSchema, PersonalInfoValues } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
+import { Form, useForm } from "react-hook-form";
 
 export default function PersonalInfoForm() {
-  return <div>PersonalInfoForm</div>;
+  const form = useForm<PersonalInfoValues>({
+    resolver: zodResolver(personalInfoSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      jobTitle: "",
+      city: "",
+      country: "",
+      email: "",
+      phone: "",
+    },
+  });
+  return (
+    <div className="mx-auto max-w-xl space-y-6">
+      <div className="space-y-1.5 text-center">
+        <h2 className="text-2xl font-semibold">Personal Info</h2>
+        <p className="text-sm text-muted-foreground">Tell us about yourself</p>
+        <Form>
+
+        </Form>
+      </div>
+    </div>
+  );
 }
