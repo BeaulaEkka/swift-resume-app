a1e2bfd254acba238773b804d60d028778ce3835
Your Name <ekka.beaula@gmail.com>
2025-02-01 00:19:35 +0100

    upload image

--- src/app/(main)/editor/forms/PersonalInfoForm.tsx
+++ src/app/(main)/editor/forms/PersonalInfoForm.tsx
@@ -1,9 +1,16 @@
-import { FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
+"use client";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { personalInfoSchema, PersonalInfoValues } from "@/lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
-import { Form, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 export default function PersonalInfoForm() {
   const form = useForm<PersonalInfoValues>({
@@ -23,22 +30,28 @@ export default function PersonalInfoForm() {
       <div className="space-y-1.5 text-center">
         <h2 className="text-2xl font-semibold">Personal Info</h2>
         <p className="text-sm text-muted-foreground">Tell us about yourself</p>
-        <Form>
-          <form {...form}>
+        <Form {...form}>
+          <form>
             <FormField
-            control={form.control}
-            name="photo"
-            render={((field))=>{
-              <FormItem>
-                <FormLabel>Your Photo</FormLabel>
-                <FormControl>
-                  <Input type="file" {...field} />
-                </FormControl>
-              </FormItem>
-            }</form>}
-            >
-
-            </FormField>
+              control={form.control}
+              name="photo"
+              render={({ field: { value, ...fieldValues } }) => (
+                <FormItem>
+                  <FormLabel>Your Photo</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="file"
+                      {...fieldValues}
+                      accept="images/"
+                      onChange={(e) => {
+                        const file = e.target.files?.[0];
+                        fieldValues.onChange(file);
+                      }}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
           </form>
         </Form>
       </div>
