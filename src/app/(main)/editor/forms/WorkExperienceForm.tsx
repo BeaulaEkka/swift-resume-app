164c2d87d5902c4fcde38de9ff453ad9596cc24b
Your Name <ekka.beaula@gmail.com>
2025-02-04 01:21:33 +0100

    WorkExperienceForm-start

---
+++ src/app/(main)/editor/forms/WorkExperienceForm.tsx
@@ -0,0 +1,10 @@
+import { workExperienceValues } from "@/lib/validation";
+import React from "react";
+
+export default function WorkExperienceForm({
+  resumeData,
+  setResumeData,
+}: EditorFormProps) {
+  const form = useForm<workExperienceValues>({});
+  return <div></div>;
+}
