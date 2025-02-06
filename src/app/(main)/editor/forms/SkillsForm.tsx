cd52b2a2abc217993d17fcbe3ac2397127cf8698
Your Name <ekka.beaula@gmail.com>
2025-02-06 23:54:36 +0100

    skillsForm

---
+++ src/app/(main)/editor/forms/SkillsForm.tsx
@@ -0,0 +1,35 @@
+import { EditorFormProps } from "@/lib/types";
+import { skillsSchema, SkillsValues } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+export default function SkillsForm({
+  resumeData,
+  setResumeData,
+}: EditorFormProps) {
+  const form = useForm<SkillsValues>({
+    resolver: zodResolver(skillsSchema),
+    defaultValues: { skills: resumeData.skills || [] },
+  });
+
+  useEffect(() => {
+    const { unsubscribe } = form.watch(async (values) => {
+      const isValid = await form.trigger();
+      if (!isValid) return;
+
+      setResumeData({
+        ...resumeData,
+        skills:
+          values.skills?.filter((skill) => skill !== undefined) || [],
+      });
+    });
+    return unsubscribe;
+  }, [form, resumeData, setResumeData]);
+
+  const { fields, append, remove } = useFieldArray({
+    control: form.control,
+    name: "workExperiences",
+  });
+  return <div></div>;
+}
