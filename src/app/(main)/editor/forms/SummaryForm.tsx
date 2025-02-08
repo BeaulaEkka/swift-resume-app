e669dd04d175e7c41e2979fd92da66a308b08087
Your Name <ekka.beaula@gmail.com>
2025-02-08 22:43:40 +0100

    summaryForm-complete

--- src/app/(main)/editor/forms/SummaryForm.tsx
+++ src/app/(main)/editor/forms/SummaryForm.tsx
@@ -1,5 +1,70 @@
-import React from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { EditorFormProps } from "@/lib/types";
+import { SummaryValues } from "@/lib/validation";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
 
-export default function SummaryForm() {
-  return <div></div>;
+export default function SummaryForm({
+  resumeData,
+  setResumeData,
+}: EditorFormProps) {
+  const form = useForm<SummaryValues>({
+    defaultValues: {
+      summary: resumeData.summary,
+    },
+  });
+
+  useEffect(() => {
+    const { unsubscribe } = form.watch(async (values) => {
+      const isValid = await form.trigger();
+      if (!isValid) return;
+
+      setResumeData({
+        ...resumeData,
+        ...values,
+      });
+    });
+
+    return unsubscribe;
+  }, [form, resumeData, setResumeData]);
+
+  return (
+    <div className="mx-auto max-w-xl space-y-6">
+      <div className="space-y-1.5 text-center">
+        <h2 className="text-2xl font-semibold">Professional Summary</h2>
+        <p className="text-sm text-muted-foreground">
+          Write a short introduction for your resume or let the AI generate one
+          from your entered data
+        </p>
+      </div>
+      <Form {...form}>
+        <form action="">
+          <FormField
+            control={form.control}
+            name="summary"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="sr-only"></FormLabel>
+                <FormControl>
+                  <Textarea
+                    {...field}
+                    placeholder="A bried description of your professional Summary"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
 }
