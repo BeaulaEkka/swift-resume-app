f9a17ece24e52daa4237694b094c25c0c38e0c85
Your Name <ekka.beaula@gmail.com>
2025-02-20 15:33:35 +0100

    ColorPicker

--- src/app/(main)/editor/ColorPicker.tsx
+++ src/app/(main)/editor/ColorPicker.tsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { Popover, PopoverTrigger } from "@/components/ui/popover";
+import { PaletteIcon } from "lucide-react";
+import React, { useState } from "react";
+import { Color, ColorChangeHandler } from "react-color";
 
-export default function ColorPicker() {
-  return <div></div>;
+interface ColorPickerProps {
+  color: Color | undefined;
+  onChange: ColorChangeHandler;
+}
+export default function ColorPicker({ color, onChange }: ColorPickerProps) {
+  const [showPopover, setshowPopover] = useState(false);
+  return (
+    <Popover open={showPopover} onOpenChange={setshowPopover}>
+      <PopoverTrigger asChild>
+        <Button>
+          <PaletteIcon />
+        </Button>
+      </PopoverTrigger>
+    </Popover>
+  );
 }
