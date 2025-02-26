d2af45d3020d9928900ea00d87bca7473c5bef44
Your Name <ekka.beaula@gmail.com>
2025-02-26 01:23:42 +0100

    borderStyleButton-stable

--- src/app/(main)/editor/ResumePreviewSection.tsx
+++ src/app/(main)/editor/ResumePreviewSection.tsx
@@ -4,18 +4,23 @@ import React from "react";
 import ColorPicker from "./ColorPicker";
 import { colors } from "@clerk/themes/dist/clerk-js/src/ui/foundations/colors";
 import BorderStyleButton from "./BorderStyleButton";
+import { cn } from "@/lib/utils";
 
 interface ResumePreviewSectionProps {
   resumeData: ResumeValues;
   setResumeData: (data: ResumeValues) => void;
+  className?: string;
 }
 export default function ResumePreviewSection({
   resumeData,
   setResumeData,
+  className,
 }: ResumePreviewSectionProps) {
   return (
-    <div className="group relative hidden md:flex">
-      <div className="lg-left-3 lg-top-3 absolute left-1 flex flex-none flex-col gap-3 opacity-50 xl:opacity-100 group-hover:opacity-100">
+    <div
+      className={cn("group relative hidden w-full md:flex md:w-1/2", className)}
+    >
+      <div className="lg-left-3 lg-top-3 absolute left-1 flex flex-none flex-col gap-3 opacity-50 transition-opacity group-hover:opacity-100 xl:opacity-100">
         <ColorPicker
           color={resumeData.colorHex}
           onChange={(color) =>
@@ -30,10 +35,7 @@ export default function ResumePreviewSection({
         />
       </div>
       <div className="flex w-full justify-center overflow-y-auto bg-secondary p-3">
-        <ResumePreview
-          resumeData={resumeData}
-          className="max-w-2xl border "
-        />
+        <ResumePreview resumeData={resumeData} className="max-w-2xl border" />
       </div>
     </div>
   );
