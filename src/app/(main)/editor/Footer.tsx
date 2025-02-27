e0fc9941141d64d1bed7b136389b6a089cd96d2b
Your Name <ekka.beaula@gmail.com>
2025-02-28 00:34:52 +0100

    resume preview

--- src/app/(main)/editor/Footer.tsx
+++ src/app/(main)/editor/Footer.tsx
@@ -2,14 +2,20 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
 import { steps } from "./steps";
+import { FileUserIcon, PenLineIcon } from "lucide-react";
 
 interface FooterProps {
   currentStep: string;
   setCurrentStep: (step: string) => void;
   showSmResumePreview: boolean;
-  setshowResumePreview: (show: boolean) => void;
+  setShowSmResumePreview: (show: boolean) => void;
 }
-export default function Footer({ currentStep, setCurrentStep }: FooterProps) {
+export default function Footer({
+  currentStep,
+  setCurrentStep,
+  showSmResumePreview,
+  setShowSmResumePreview,
+}: FooterProps) {
   const previousStep = steps.find(
     (_, index) => steps[index + 1]?.key === currentStep,
   )?.key;
@@ -39,6 +45,17 @@ export default function Footer({ currentStep, setCurrentStep }: FooterProps) {
             Next Step
           </Button>
         </div>
+        <Button
+          variant="outline"
+          size="icon"
+          onClick={() => {
+            setShowSmResumePreview(!showSmResumePreview);
+          }}
+          className="md:hidden"
+          title="Show resume preview"
+        >
+          {showSmResumePreview ? <PenLineIcon /> : <FileUserIcon />}
+        </Button>
         <div className="flex items-center gap-3">
           <Button variant="secondary" asChild>
             <Link href="/resumes">Close</Link>
