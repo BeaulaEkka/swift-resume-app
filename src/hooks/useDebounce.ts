c60f05e31c725ce44e45eac658272440eef767ff
Your Name <ekka.beaula@gmail.com>
2025-03-04 15:29:24 +0100

    useDebounce-done

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+export default function useDebounce<T>(value: T, delay: number = 250) {
+  const [debounceValue, setDebouncevalue] = useState<T>(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncevalue(value);
+    }, delay);
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return debounceValue;
+}
