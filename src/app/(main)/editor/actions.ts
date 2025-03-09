d19e0dc3e84c8a891d12511ff99db5941639536c
Your Name <ekka.beaula@gmail.com>
2025-03-09 23:55:57 +0100

    saveResume

--- src/app/(main)/editor/actions.ts
+++ src/app/(main)/editor/actions.ts
@@ -1,22 +1,62 @@
-'use server'
+"use server";
 
-import { resumeSchema, ResumeValues } from '@/lib/validation'
-import { auth } from '@clerk/nextjs/server'
+import prisma from "@/lib/prisma";
+import { resumeSchema, ResumeValues } from "@/lib/validation";
+import { auth } from "@clerk/nextjs/server";
 
+export async function saveResume(values: ResumeValues) {
+  const { id } = values;
+  console.log("recieved values", values);
 
-export async function saveResume(values:ResumeValues) {
-    const {id}=values
-    console.log("recieved values",values)
+  const { photo, workExperiences, educations, ...resumeValues } =
+    resumeSchema.parse(values);
 
-    const {photo,workExperiences,educations,...resumeValues}=resumeSchema.parse(values)
+  const { userId } = await auth();
 
-    const{}=await auth()
+  if (!userId) {
+    throw new Error("User not authenticated");
+  }
 
+  //TODOcheck resume count for non-premium users
 
+  const existingResume = id
+    ? await prisma.resume.findUnique({ where: { id, userId } })
+    : null;
 
-  return (
-    <div>
-      
-    </div>
-  )
+  if (id && !existingResume) {
+    throw new Error("Resume not found");
+  }
+
+  let newPhotoUrl: string | undefined | null = undefined;
+
+  if (photo instanceof File) {
+    if (existingResume?.photoUrl) {
+      await del(existingResume.photoUrl);
+    }
+
+    const blob = await Put(`resume_photos/${path.extname(photo.name)}`, photo, {
+      access: "public",
+    });
+
+    newPhotoUrl: blob.url;
+  } else if (photo === null) {
+    if (existingResume?.photoUrl) {
+      await del(existingResume.photoUrl);
+    }
+    newPhotoUrl = null;
+  }
+
+  // return (
+  //   <div>
+
+  //   </div>
+  // )
+  if(id){return prisma.resume.update({
+    where:{id},
+    data:{
+      ...resumeValues,
+      photoUrl:newPhotoUrl,
+      workExperiences:workExperiences.map((experience)=>{}
+    }
+  })}
 }
