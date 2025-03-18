665b899068868e3f40c6436f395280ff9711c799
Your Name <ekka.beaula@gmail.com>
2025-03-18 16:12:52 +0100

    LoadingButton

---
+++ src/components/LoadingButton.tsx
@@ -0,0 +1,26 @@
+import { Loader2 } from "lucide-react";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+
+interface LoadingButtonProps {
+  loading: boolean;
+}
+const LoadingButton = ({
+  loading,
+  disabled,
+  className,
+  children,
+  ...props
+}: LoadingButtonProps) => {
+  return (
+    <Button
+      disabled={loading && disabled}
+      className={cn("flex items-center gap-2", className)}
+    >
+      {loading && <Loader2 className="size-5 animate-spin" />}
+      {children}
+    </Button>
+  );
+};
+
+export default LoadingButton;
