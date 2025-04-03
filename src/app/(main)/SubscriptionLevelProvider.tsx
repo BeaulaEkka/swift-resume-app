b80052af1719e9b2b80f2aafc82485f498934850
Your Name <ekka.beaula@gmail.com>
2025-04-03 23:28:47 +0200

    SubscriptionLevelContext

--- src/app/(main)/SubscriptionLevelProvider.tsx
+++ src/app/(main)/SubscriptionLevelProvider.tsx
@@ -1,13 +1,26 @@
-'use client'
+"use client";
 
-import React from 'react'
+import { SubscriptionLevel } from "@/lib/subscriptions";
+import React from "react";
+import { createContext } from "react";
 
-const SubscriptionLevelProvider = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const SubscriptionLevelContext = createContext<SubscriptionLevel | undefined>(
+  undefined,
+);
+
+interface SubscriptionLevelProviderProps {
+  children: React.ReactNode;
+  userSubscriptionLevel: SubscriptionLevel;
 }
+const SubscriptionLevelProvider = ({
+  children,
+  userSubscriptionLevel,
+}: SubscriptionLevelProviderProps) => {
+  return (
+    <SubscriptionLevelContext.Provider value={userSubscriptionLevel}>
+      {children}
+    </SubscriptionLevelContext.Provider>
+  );
+};
 
-export default SubscriptionLevelProvider
+export default SubscriptionLevelProvider;
