5a99fcaa72156509bf922e7513d136ecf92cbfcf
Your Name <ekka.beaula@gmail.com>
2025-04-04 20:59:58 +0200

    canCreateResume

--- src/lib/permissions.ts
+++ src/lib/permissions.ts
@@ -1,1 +1,16 @@
-export function canCreateResume() {}
+import { SubscriptionLevel } from "./subscriptions";
+
+export function canCreateResume(
+  subscriptionLevel: SubscriptionLevel,
+  currentResumeCount: number
+) {
+  const maxResumeMap: Record<SubscriptionLevel, number> = {
+    free: 1,
+    pro: 5,
+    pro_plus: Infinity,
+  };
+
+  const maxResumes = maxResumeMap(subscriptionLevel);
+
+  return currentResumeCount < maxResumes;
+}
