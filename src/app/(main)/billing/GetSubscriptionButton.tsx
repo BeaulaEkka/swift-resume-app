3227636790ec5c409707dd71f29dd146acfb218c
Your Name <ekka.beaula@gmail.com>
2025-04-15 23:36:50 +0200

    ManageSubscriptionButton

--- src/app/(main)/billing/GetSubscriptionButton.tsx
+++ src/app/(main)/billing/GetSubscriptionButton.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
+import usePremiumModal from "@/hooks/usePremiumModal";
 import React from "react";
 
 export default function GetSubscriptionButton() {
-  return <div></div>;
+  const premiumModal = usePremiumModal();
+  return (
+    <Button variant="premium" onClick={() => premiumModal.setOpen(true)}>
+      Get Premium Subscription
+    </Button>
+  );
 }
