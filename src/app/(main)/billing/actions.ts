9005ace003b3ad37da2779468843e0af589e7693
Your Name <ekka.beaula@gmail.com>
2025-04-16 12:14:55 +0200

    ManageSubscriptionButton

---
+++ src/app/(main)/billing/actions.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import stripe from "@/lib/stripe";
+import { currentUser } from "@clerk/nextjs/server";
+
+export async function createCustomerPoralSession() {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const stripeCustomerId = user.publicMetadata.stripeCustomer as
+    | string
+    | undefined;
+  if (!stripeCustomerId) {
+    throw new Error("Stripe customer ID not found");
+  }
+
+  const session = await stripe.billingPortal.sessions.create({
+    customer: stripeCustomerId,
+    return_url: process.env.NEXT_PUBLIC_SITE_URL,
+  });
+  if (!session.url) {
+    throw new Error("Session URL not found");
+  }
+  return session.url;
+}
--- src/app/(main)/billing/success/ManageSubscriptionButton.tsx
+++ src/app/(main)/billing/success/ManageSubscriptionButton.tsx
@@ -1,5 +1,33 @@
 "use client";
 
+import LoadingButton from "@/components/LoadingButton";
+import { useToast } from "@/hooks/use-toast";
+import { useState } from "react";
+
 export default function ManageSubscriptionButton() {
-  return <div></div>;
+  const { toast } = useToast();
+
+  const [loading, setLoading] = useState(false);
+
+  async function handleClick() {
+    try {
+    } catch (error) {
+      console.error(error);
+      toast({
+        title: "Error",
+        description: "An error occurred while managing your subscription.",
+        variant: "destructive",
+      });
+
+      console.error("Error managing subscription:", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <LoadingButton onClick={handleClick} loading={loading}>
+      Manage Subscription
+    </LoadingButton>
+  );
 }
