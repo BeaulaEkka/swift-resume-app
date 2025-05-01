8c9d908dbb2bdd4943b2c374979619ac45c3dd93
Your Name <ekka.beaula@gmail.com>
2025-05-01 23:13:34 +0200

    fixed build issues around colorPicker Types

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,63 +1,3 @@
-// import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
-// import "./globals.css";
-
-// import {
-//   ClerkProvider,
-//   SignedIn,
-//   SignedOut,
-//   SignInButton,
-//   UserButton,
-//   // SignedIn,
-//   // SignedOut,
-//   // SignInButton,
-//   // UserButton,
-// } from "@clerk/nextjs";
-// import { ThemeProvider } from "next-themes";
-// import { Toaster } from "@/components/ui/toaster";
-// import Navbar from "./(main)/Navbar";
-
-// const inter = Inter({
-//   subsets: ["latin"],
-// });
-
-// export const metadata: Metadata = {
-//   title: "Swift Resume Builder",
-//   description: "AI ResumeBuilder",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <ClerkProvider>
-//       <html lang="en" suppressHydrationWarning>
-//         <body className={inter.className}>
-//           <ThemeProvider
-//             attribute="class"
-//             defaultTheme="system"
-//             enableSystem
-//             disableTransitionOnChange
-//           >
-//             <SignedOut>
-//               <SignInButton />{" "}
-//             </SignedOut>
-//             <SignedIn>
-//               <UserButton />
-//             </SignedIn>
-
-//             <Navbar />
-//             {children}
-//             <Toaster />
-//           </ThemeProvider>
-//         </body>
-//       </html>
-//     </ClerkProvider>
-//   );
-// }
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -73,6 +13,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Swift Resume Builder",
   description: "AI ResumeBuilder",
+  metadataBase: new URL("https://resumeai-builder.vercel.app"),
 };
 
 export default function RootLayout({
